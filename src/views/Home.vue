88b74ee7559bfb82cef4f4a5bd9db1358fed58e8
nomachalle <nomachalle.com@gmail.com>
2021-04-04 09:31:05 +0900

    確認テストの回答です

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <router-link :to="{name: 'Member', prams: { id: 1 }}">
+          1へ進む
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'Member', prams: { id: 2 }}">
+          2へ進む
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="{name: 'Member', prams: { id: 3 }}">
+          3へ進む
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+ul {
+  padding: 0px;
+}
+li {
+  list-style: none;
+}
+</style>
